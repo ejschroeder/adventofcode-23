58702ec341c3668095bc503ec8dc3177c96b5cb8
E.J. Schroeder <ej.schr@gmail.com>
2023-12-11 12:58:31 -0500

    clean up day 11

--- src/main/kotlin/lol/schroeder/aoc23/Day11.kt
+++ src/main/kotlin/lol/schroeder/aoc23/Day11.kt
@@ -4,54 +4,35 @@ import kotlin.math.max
 import kotlin.math.min
 
 class Day11(private val input: List<String> = readInputLines("day11")) : Day() {
-    data class Coordinate(val row: Int, val col: Int)
-    override fun part1(): Any {
-        val galaxies = input.flatMapIndexed { rowIdx, row -> row.withIndex()
-                .filter { it.value == '#' }
-                .map { Coordinate(rowIdx, it.index) }
-        }
+    private val galaxies = parseGalaxies()
+    private val occupiedCols = galaxies.map { it.col }.fold(setOf(), Set<Int>::plus)
+    private val occupiedRows = galaxies.map { it.row }.fold(setOf(), Set<Int>::plus)
 
-        val occupiedCols = galaxies.fold(mutableSetOf<Int>()) { columns, galaxy -> columns.apply { add(galaxy.col) } }
-        val occupiedRows = galaxies.fold(mutableSetOf<Int>()) { columns, galaxy -> columns.apply { add(galaxy.row) } }
+    override fun part1() = galaxies.elementPairs()
+        .sumOf { (first, second) -> getDistanceBetween(first, second) }
 
-        return galaxies.elementPairs()
-            .map { (first, second) -> getDistanceBetween(first, second, occupiedCols, occupiedRows) }
-            .sum()
-    }
+    override fun part2() = galaxies.elementPairs()
+        .sumOf { (first, second) -> getDistanceBetween(first, second, expansionSize = 1_000_000) }
 
-    private fun getDistanceBetween(
-        first: Coordinate,
-        second: Coordinate,
-        occupiedCols: Set<Int>,
-        occupiedRows: Set<Int>,
-        expansionSize: Long = 2
-    ): Long {
+    private fun getDistanceBetween(first: Coordinate, second: Coordinate, expansionSize: Long = 2): Long {
         val minRow = min(first.row, second.row)
         val maxRow = max(first.row, second.row)
-
         val rowDistance = (minRow..<maxRow).sumOf { if (it in occupiedRows) 1 else expansionSize }
 
         val minCol = min(first.col, second.col)
         val maxCol = max(first.col, second.col)
-
         val colDistance = (minCol..<maxCol).sumOf { if (it in occupiedCols) 1 else expansionSize }
 
         return rowDistance + colDistance
     }
 
-    override fun part2(): Any {
-        val galaxies = input.flatMapIndexed { rowIdx, row -> row.withIndex()
+    private fun parseGalaxies() = input.flatMapIndexed { rowIdx, row ->
+        row.withIndex()
             .filter { it.value == '#' }
             .map { Coordinate(rowIdx, it.index) }
-        }
-
-        val occupiedCols = galaxies.fold(mutableSetOf<Int>()) { columns, galaxy -> columns.apply { add(galaxy.col) } }
-        val occupiedRows = galaxies.fold(mutableSetOf<Int>()) { columns, galaxy -> columns.apply { add(galaxy.row) } }
-
-        return galaxies.elementPairs()
-            .map { (first, second) -> getDistanceBetween(first, second, occupiedCols, occupiedRows, 1_000_000) }
-            .sum()
     }
+
+    data class Coordinate(val row: Int, val col: Int)
 }
 
 fun main() = Day11().solve()
