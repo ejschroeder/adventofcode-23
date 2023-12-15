9c46767bfced3bc7e55a1128726b1c37bc654b1d
E.J. Schroeder <ej.schr@gmail.com>
2023-12-15 12:33:40 -0500

    clean up day 15

--- src/main/kotlin/lol/schroeder/aoc23/Day15.kt
+++ src/main/kotlin/lol/schroeder/aoc23/Day15.kt
@@ -1,50 +1,22 @@
 package lol.schroeder.aoc23
 
 class Day15(private val input: List<String> = readInputLines("day15")) : Day() {
-    sealed class Operation {
-        data class Add(val lens: Lens) : Operation()
-        data class Remove(val label: String): Operation()
-    }
-
-    data class Lens(val label: String, val focalLength: Int) {
-        val hash: Int get() = label.fold(0) { acc, c -> ((acc + c.code) * 17) % 256 }
-    }
     override fun part1(): Any {
-        return input.flatMap { it.split(",") }.sumOf { it.hash() }
+        return input.flatMap { it.split(",") }
+            .sumOf { it.fold(0) { acc, c -> ((acc + c.code) * 17) % 256 }.toInt() }
     }
 
     override fun part2(): Any {
-        val map = List(256) { mutableListOf<Lens>() }
-
-        val sorted = input.flatMap { it.split(",") }
+        val lensConfiguration = input.flatMap { it.split(",") }
             .map(::toOperation)
-            .fold(map, ::applyOperation)
+            .fold(LensConfiguration(), ::applyOperation)
 
-        val focusingPower = sorted.withIndex().sumOf { (boxId, box) ->
-            box.withIndex().sumOf { (slotId, lens) ->
-                (1 + boxId) * (1 + slotId) * lens.focalLength
-            }
-        }
-
-        return focusingPower
+        return lensConfiguration.focusingPower
     }
 
-    private fun applyOperation(map: List<MutableList<Lens>>, op: Operation) = when (op) {
-        is Operation.Add -> add(map, op.lens)
-        is Operation.Remove -> remove(map, op.label)
-    }
-
-    private fun add(map: List<MutableList<Lens>>, lens: Lens): List<MutableList<Lens>> {
-        val idx = lens.label.hash()
-        val foundIdx = map[idx].indexOfFirst { it.label == lens.label }
-        if (foundIdx < 0) map[idx].add(lens) else map[idx][foundIdx] = lens
-        return map
-    }
-
-    private fun remove(map: List<MutableList<Lens>>, label: String): List<MutableList<Lens>> {
-        val idx = label.hash()
-        map[idx].removeAll { it.label == label }
-        return map
+    private fun applyOperation(lensConfiguration: LensConfiguration, op: Operation) = when (op) {
+        is Operation.Add -> lensConfiguration.add(op.lens)
+        is Operation.Remove -> lensConfiguration.remove(op.label)
     }
 
     private fun toOperation(str: String): Operation {
@@ -56,7 +28,37 @@ class Day15(private val input: List<String> = readInputLines("day15")) : Day() {
         return Operation.Add(Lens(label, focalLength.toInt()))
     }
 
-    private fun String.hash() = this.fold(0) { acc, c -> ((acc + c.code) * 17) % 256 }
+    sealed class Operation {
+        data class Add(val lens: Lens) : Operation()
+        data class Remove(val label: String): Operation()
+    }
+
+    data class Lens(val label: String, val focalLength: Int)
+
+    class LensConfiguration {
+        private val boxes = List(256) { mutableListOf<Lens>() }
+
+        val focusingPower: Int get() = boxes.withIndex().sumOf { (boxId, box) ->
+            box.withIndex().sumOf { (slotId, lens) ->
+                (1 + boxId) * (1 + slotId) * lens.focalLength
+            }
+        }
+
+        fun add(lens: Lens): LensConfiguration {
+            val idx = lens.label.hash()
+            val foundIdx = boxes[idx].indexOfFirst { it.label == lens.label }
+            if (foundIdx < 0) boxes[idx].add(lens) else boxes[idx][foundIdx] = lens
+            return this
+        }
+
+        fun remove(label: String): LensConfiguration {
+            val idx = label.hash()
+            boxes[idx].removeAll { it.label == label }
+            return this
+        }
+
+        private fun String.hash() = this.fold(0) { acc, c -> ((acc + c.code) * 17) % 256 }
+    }
 }
 
 fun main() = Day15().solve()
