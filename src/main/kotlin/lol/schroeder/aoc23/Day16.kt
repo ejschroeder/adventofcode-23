360554e8e9d17e4a5b192c9be2475c2495a41cc9
E.J. Schroeder <ej.schr@gmail.com>
2023-12-16 10:36:51 -0500

    cleanup

--- src/main/kotlin/lol/schroeder/aoc23/Day16.kt
+++ src/main/kotlin/lol/schroeder/aoc23/Day16.kt
@@ -1,42 +1,6 @@
 package lol.schroeder.aoc23
 
 class Day16(private val input: List<String> = readInputLines("day16")) : Day() {
-    enum class Dir {
-        UP, RIGHT, DOWN, LEFT
-    }
-
-    data class Laser(val row: Int, val col: Int, val dir: Dir) {
-
-        fun next(tile: Char) = when (tile) {
-            '\\' -> when(dir) {
-                Dir.UP -> listOf(copy(dir = Dir.LEFT).straight())
-                Dir.RIGHT -> listOf(copy(dir = Dir.DOWN).straight())
-                Dir.DOWN -> listOf(copy(dir = Dir.RIGHT).straight())
-                Dir.LEFT -> listOf(copy(dir = Dir.UP).straight())
-            }
-            '/' -> when(dir) {
-                Dir.UP -> listOf(copy(dir = Dir.RIGHT).straight())
-                Dir.RIGHT -> listOf(copy(dir = Dir.UP).straight())
-                Dir.DOWN -> listOf(copy(dir = Dir.LEFT).straight())
-                Dir.LEFT -> listOf(copy(dir = Dir.DOWN).straight())
-            }
-            '-' -> when (dir) {
-                Dir.UP, Dir.DOWN -> listOf(copy(dir = Dir.LEFT).straight(), copy(dir = Dir.RIGHT).straight())
-                else -> listOf(straight())
-            }
-            '|' -> when (dir) {
-                Dir.LEFT, Dir.RIGHT -> listOf(copy(dir = Dir.UP).straight(), copy(dir = Dir.DOWN).straight())
-                else -> listOf(straight())
-            }
-            else -> listOf(straight())
-        }
-        private fun straight() = when (dir) {
-            Dir.UP -> copy(row = row - 1)
-            Dir.RIGHT -> copy(col = col + 1)
-            Dir.DOWN -> copy(row = row + 1)
-            Dir.LEFT -> copy(col = col - 1)
-        }
-    }
 
     override fun part1(): Any {
         val grid = input.map { it.toList() }
@@ -60,17 +24,65 @@ class Day16(private val input: List<String> = readInputLines("day16")) : Day() {
     private fun countEnergizedTiles(grid: List<List<Char>>, start: Laser): Int {
         val queue = ArrayDeque<Laser>().apply { add(start) }
         val seen = hashSetOf<Laser>()
-        while (queue.isNotEmpty()) {
-            val laser = queue.removeFirst()
-            seen.add(laser)
-            val tile = grid[laser.row][laser.col]
 
-            laser.next(tile)
-                .filter { it !in seen && it.row in grid.indices && it.col in grid[it.row].indices }
-                .forEach(queue::add)
+        return countEnergizedTiles(grid, queue, seen)
+    }
+
+    private tailrec fun countEnergizedTiles(grid: List<List<Char>>, queue: ArrayDeque<Laser>, seen: MutableSet<Laser>): Int {
+        if (queue.isEmpty()) return seen.distinctBy { it.row to it.col }.count()
+
+        val laser = queue.removeFirst()
+        seen.add(laser)
+
+        val nextTiles = laser.next(grid[laser.row][laser.col])
+            .filter { it !in seen && it.row in grid.indices && it.col in grid[it.row].indices }
+
+        return countEnergizedTiles(grid, queue.apply { addAll(nextTiles) }, seen)
+    }
+
+    enum class Dir {
+        UP, RIGHT, DOWN, LEFT
+    }
+
+    data class Laser(val row: Int, val col: Int, val dir: Dir) {
+
+        fun next(tile: Char) = when (tile) {
+            '\\' -> when(dir) {
+                Dir.UP -> listOf(turnAndMove(Dir.LEFT))
+                Dir.RIGHT -> listOf(turnAndMove(Dir.DOWN))
+                Dir.DOWN -> listOf(turnAndMove(Dir.RIGHT))
+                Dir.LEFT -> listOf(turnAndMove(Dir.UP))
+            }
+            '/' -> when(dir) {
+                Dir.UP -> listOf(turnAndMove(Dir.RIGHT))
+                Dir.RIGHT -> listOf(turnAndMove(Dir.UP))
+                Dir.DOWN -> listOf(turnAndMove(Dir.LEFT))
+                Dir.LEFT -> listOf(turnAndMove(Dir.DOWN))
+            }
+            '-' -> when (dir) {
+                Dir.UP, Dir.DOWN -> listOf(turnAndMove(Dir.LEFT), turnAndMove(Dir.RIGHT))
+                else -> listOf(straight())
+            }
+            '|' -> when (dir) {
+                Dir.LEFT, Dir.RIGHT -> listOf(turnAndMove(Dir.UP), turnAndMove(Dir.DOWN))
+                else -> listOf(straight())
+            }
+            else -> listOf(straight())
         }
 
-        return seen.distinctBy { it.row to it.col }.count()
+        private fun turnAndMove(dir: Dir) = when (dir) {
+            Dir.UP -> copy(row = row - 1, dir = dir)
+            Dir.RIGHT -> copy(col = col + 1, dir = dir)
+            Dir.DOWN -> copy(row = row + 1, dir = dir)
+            Dir.LEFT -> copy(col = col - 1, dir = dir)
+        }
+
+        private fun straight() = when (dir) {
+            Dir.UP -> copy(row = row - 1)
+            Dir.RIGHT -> copy(col = col + 1)
+            Dir.DOWN -> copy(row = row + 1)
+            Dir.LEFT -> copy(col = col - 1)
+        }
     }
 }
 
