04555c39f7face22d3e917226bd66f0ad2ac84f9
E.J. Schroeder <ej.schr@gmail.com>
2023-12-16 10:17:30 -0500

    day 16 part 1

---
+++ src/test/kotlin/lol/schroeder/aoc23/Day16Test.kt
@@ -0,0 +1,34 @@
+package lol.schroeder.aoc23
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+
+class Day16Test {
+
+    @Test
+    fun part1() {
+        val input = """
+            .|...\....
+            |.-.\.....
+            .....|-...
+            ........|.
+            ..........
+            .........\
+            ..../.\\..
+            .-.-/..|..
+            .|....-|.\
+            ..//.|....
+        """.trimIndent().lines()
+
+        val result = Day16(input).part1()
+
+        expectThat(result) isEqualTo 46
+    }
+
+    @Test
+    fun part2() {
+    }
+}
